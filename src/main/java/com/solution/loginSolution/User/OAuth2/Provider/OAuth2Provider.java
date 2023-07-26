9616ec989ec85ab4e4d6e2205378d96cc28997a8
seate <86824703+seate@users.noreply.github.com>
2023-07-27 04:44:21 +0900

    OAuth2 naver 로그인 구현

--- src/main/java/com/solution/loginSolution/User/OAuth2/Provider/OAuth2Provider.java
+++ src/main/java/com/solution/loginSolution/User/OAuth2/Provider/OAuth2Provider.java
@@ -3,6 +3,7 @@ package com.solution.loginSolution.User.OAuth2.Provider;
 import com.solution.loginSolution.User.OAuth2.Exception.AuthServerNotSupportedException;
 import com.solution.loginSolution.User.OAuth2.OAuth2UserAttributes.GoogleOAuth2UserAttributes;
 import com.solution.loginSolution.User.OAuth2.OAuth2UserAttributes.KakaoOAuth2UserAttributes;
+import com.solution.loginSolution.User.OAuth2.OAuth2UserAttributes.NaverOAuth2UserAttributes;
 import com.solution.loginSolution.User.OAuth2.OAuth2UserAttributes.OAuth2UserAttributes;
 import lombok.Getter;
 
@@ -15,10 +16,10 @@ public class OAuth2Provider {
     public static OAuth2UserAttributes distinguish(String registrationId, Map<String, Object> attributes)
             throws AuthServerNotSupportedException {
         return switch (registrationId) {
-            //TODO kakao, naver
+            //TODO naver
             case "google" -> new GoogleOAuth2UserAttributes(attributes);
             case "kakao" -> new KakaoOAuth2UserAttributes(attributes);
-            //case "naver" -> new NaverOauth2UserInfo(attributes);
+            case "naver" -> new NaverOAuth2UserAttributes(attributes);
             default -> throw new AuthServerNotSupportedException();
         };
     }
