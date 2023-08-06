23bebed5dee6625bc0410e348809c0e55e33f2ae
seate <86824703+seate@users.noreply.github.com>
2023-08-06 10:45:21 +0900

    fix: timeStamp apply config fix

--- src/main/java/com/project/githering/Common/Config/SpringConfig.java
+++ src/main/java/com/project/githering/Common/Config/SpringConfig.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
+@EnableJpaAuditing
 public class SpringConfig {
 
     @Bean
